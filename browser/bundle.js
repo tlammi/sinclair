ae5dc6f940c782abe593ec03632c65c6fc2eff61
tlammi <tangojulietlima@gmail.com>
2023-03-12 17:18:25 +0200

    Start inserting HTML

--- browser/bundle.js
+++ browser/bundle.js
@@ -401,7 +401,26 @@
   };
 
   console.log(String(sinclair_coeff(1, 2, 100)));
-  var hello = el("h1", "hello, world");
-  mount(document.body, hello);
+  var sinclair_score = document.getElementById("sinclair_score");
+  var sinclair_kg = document.getElementById("sinclair_kg");
+  var sinclair_bw = document.getElementById("sinclair_bw");
+  if (sinclair_score) {
+    var hello = el("h1", "Sinclair Score here");
+    mount(sinclair_score, hello);
+  } else {
+    console.log("sinclair_score not found");
+  }
+  if (sinclair_kg) {
+    var hello = el("h1", "Sinclair kg here");
+    mount(sinclair_kg, hello);
+  } else {
+    console.log("sinclair_kg not found");
+  }
+  if (sinclair_bw) {
+    var hello = el("h1", "Sinclair bw here");
+    mount(sinclair_bw, hello);
+  } else {
+    console.log("sinclair_bw not found");
+  }
 
 })();
--- sinclair.web.ts
+++ sinclair.web.ts
@@ -4,5 +4,29 @@ import {el, mount} from './redom.es';
 
 console.log(String(sinclair_coeff(1,2,100)));
 
-const hello = el("h1", "hello, world");
-mount(document.body, hello);
+
+const sinclair_score = document.getElementById("sinclair_score");
+const sinclair_kg = document.getElementById("sinclair_kg");
+const sinclair_bw = document.getElementById("sinclair_bw");
+
+if(sinclair_score){
+    let hello = el("h1", "Sinclair Score here");
+    mount(sinclair_score, hello);
+} else {
+    console.log("sinclair_score not found");
+}
+
+if(sinclair_kg){
+    let hello = el("h1", "Sinclair kg here");
+    mount(sinclair_kg, hello);
+} else {
+    console.log("sinclair_kg not found");
+}
+
+if(sinclair_bw){
+    let hello = el("h1", "Sinclair bw here");
+    mount(sinclair_bw, hello);
+} else {
+    console.log("sinclair_bw not found");
+}
+
